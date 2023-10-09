eadd59037ca3d95d9e8da78c59e38172f8fb59cc
Yuriy <yura.yuzgin@yandex.ru>
2023-10-09 21:46:27 +0300

    Сделан вывод списка постов.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+
 function App() {
-	return <div className="App"></div>;
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'Javascript 1', body: 'Description' },
+		{ id: 2, title: 'Javascript 2', body: 'Description' },
+		{ id: 3, title: 'Javascript 3', body: 'Description' },
+	]);
+
+	return (
+		<div className="App">
+			<PostList posts={posts} title={'Посты про JS'} />
+		</div>
+	);
 }
 
 export default App;
