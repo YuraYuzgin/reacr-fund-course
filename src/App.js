4680b9aec0ca1bafa58a9a89a370c845542f2d87
Yuriy <yura.yuzgin@yandex.ru>
2023-10-12 00:34:26 +0300

    Добавлена пагинация.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,30 @@ import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
+import Pagination from './components/UI/pagination/Pagination';
 
 function App() {
 	const [posts, setPosts] = useState([]);
 	const [filter, setFilter] = useState({ sort: '', query: '' });
 	const [modal, setModal] = useState(false);
-	const [isPostsLoading, setIsPostsLoading] = useState(false);
+	const [totalPages, setTotalPages] = useState(0);
+	const [limit, setLimit] = useState(10);
+	const [page, setPage] = useState(1);
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+	const [fetchPosts, isPostsLoading, postError] = useFetching(
+		async (limit, page) => {
+			const response = await PostService.getAll(limit, page);
+			setPosts(response.data);
+			const totalCount = response.headers['x-total-count'];
+			setTotalPages(getPageCount(totalCount, limit));
+		}
+	);
+
 	useEffect(() => {
-		fetchPosts();
+		fetchPosts(limit, page);
 	}, []);
 
 	const createPost = (newPost) => {
@@ -25,15 +39,13 @@ function App() {
 		setModal(false);
 	};
 
-	async function fetchPosts() {
-		setIsPostsLoading(true);
-		const posts = await PostService.getAll();
-		setPosts(posts);
-		setIsPostsLoading(false);
-	}
-
 	const removePost = (post) => {
 		setPosts(posts.filter((p) => p.id !== post.id));
+	};
+
+	const changePage = (page) => {
+		setPage(page);
+		fetchPosts(limit, page);
 	};
 
 	return (
@@ -46,6 +58,7 @@ function App() {
 			</MyModal>
 			<hr style={{ margin: '15px 0' }} />
 			<PostFilter filter={filter} setFilter={setFilter} />
+			{postError && <h1>Произошла ошибка ${postError}</h1>}
 			{isPostsLoading ? (
 				<div
 					style={{
@@ -63,6 +76,7 @@ function App() {
 					title={'Посты про JS'}
 				/>
 			)}
+			<Pagination page={page} changePage={changePage} totalPages={totalPages} />
 		</div>
 	);
 }
